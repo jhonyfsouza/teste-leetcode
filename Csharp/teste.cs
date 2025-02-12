ed7976384582b2cbd3d9100c18293ec881168932
jhony <jhony.souza@dialweb.com.br>
2025-02-12 17:48:13 -0300

    solucao de teste finalizada

---
+++ Csharp/teste.cs
@@ -0,0 +1,33 @@
+public class Solution
+{
+    public bool DetectCapitalUse(string word)
+    {
+
+        if (word.Length < 1 || word.Length > 100)
+        {
+            return false;
+        }
+
+        if (word == word.ToUpper() || word == word.ToLower())
+        {
+            return true;
+        }
+
+        if (char.IsLower(word[0]))
+        {
+            return false;
+        }
+
+        if (char.IsUpper(word[0]))
+        {
+            for (int i = 1; i < word.Length; i++)
+            {
+                if (char.IsUpper(word[i]))
+                {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+}
